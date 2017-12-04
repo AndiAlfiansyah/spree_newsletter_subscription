7ebccab0870ba42dd408e1257a48a834204345ea
AndiAlfiansyah <m.andi@41studio.com>
2017-12-04 14:40:48 +0700

    update

--- db/migrate/20141118145038_add_subscribed_to_mailing_list_to_spree_users.rb
+++ db/migrate/20141118145038_add_subscribed_to_mailing_list_to_spree_users.rb
@@ -1,4 +1,4 @@
-class AddSubscribedToMailingListToSpreeUsers < ActiveRecord::Migration
+class AddSubscribedToMailingListToSpreeUsers < ActiveRecord::Migration[5.0]
   def change
     add_column Spree.user_class.table_name, :subscribed_to_mailing_list, :boolean, null: false, default: false
   end
